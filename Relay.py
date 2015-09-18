76580bbd56c0da1e0af733775e33d29f49e60f09
2rDK <lemminger@gmail.com>
2015-09-18 17:22:06 +0200

    implemented GPIO

--- Relay.py
+++ Relay.py
@@ -1,3 +1,9 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+
+
 class Relay(object):
     """DOKUMENTATION
 
@@ -8,13 +14,16 @@ class Relay(object):
         """Return a Relay object whose name is *name* and starting
         state is TRUE (ON)"""
         self.pin = pin
+        GPIO.setup(pin, GPIO.OUT)
         self.state = False
-
+        GPIO.output(pin, self.state)
 
     def enable(self):
         self.state = True
+        GPIO.output(self.pin, self.state)
         return self.state
 
     def disable(self):
         self.state = False
+        GPIO.output(self.pin, self.state)
         return self.state
