f8b37b46efc418b82a471b330edec612a41ce297
2rDK <lemminger@gmail.com>
2015-10-07 19:40:03 +0200

    test

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+echo -e "### \e[32mInstalling prerequisites...\e[0m"
+
 apt-get install midori -y
 apt-get install python3-pip -y
 pip3 install requests
 apt-get install libmysqlclient-dev -y
 apt-get install python-dev -y
 pip3 install mysqlclient
+
+echo -e "### \e[32mSetting up cron...\e[0m"
+
+ThisFolder = pwd
+
+echo -e ThisFolder
