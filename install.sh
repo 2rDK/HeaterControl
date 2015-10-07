74b22e847394383ea9729a3228a924332f95b632
2rDK <lemminger@gmail.com>
2015-10-07 17:59:30 +0200

    Init version

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+apt-get install chromium -y
+apt-get install python3-pip -y
+pip3 install requests
+apt-get install libmysqlclient-dev -y
+apt-get install python-dev -y
+pip3 install mysqlclient
