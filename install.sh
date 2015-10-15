9ee3354e195d1b5af87ad4bc9bf52b4487a32029
2rDK <lemminger@gmail.com>
2015-10-15 19:25:37 +0200

    Add backend to installation script

--- install.sh
+++ install.sh
@@ -16,14 +16,17 @@ ThisFolder=$(pwd)
 
 echo $ThisFolder
 
-echo -en "\e[32m### Copying staging file to init.d... ###\e[0m\n"
-\cp -v $ThisFolder/install/HeaterControlCron /etc/init.d/HeaterControlCron
+echo -en "\e[32m### Copying staging files to init.d... ###\e[0m\n"
+\cp -v $ThisFolder/install/HeaterControlFront /etc/init.d/HeaterControlFront
+\cp -v $ThisFolder/install/HeaterControlBack /etc/init.d/HeaterControlBack
 
-echo -en "\e[32m### Setting execution permission... ###\e[0m\n"
-chmod +x /etc/init.d/HeaterControlCron
+echo -en "\e[32m### Setting execution permissions... ###\e[0m\n"
+chmod +x /etc/init.d/HeaterControlFront
+chmod +x /etc/init.d/HeaterControlBack
 
 echo -en "\e[32m### Adapting to current folder... ###\e[0m\n"
-sed -i -e "s|@FOLDER@|$ThisFolder|g" /etc/init.d/HeaterControlCron
-
+sed -i -e "s|@FOLDER@|$ThisFolder|g" /etc/init.d/HeaterControlFront
+sed -i -e "s|@FOLDER@|$ThisFolder|g" /etc/init.d/HeaterControlBack
 echo -en "\e[32m### Adding to boot sequence... ###\e[0m\n"
-update-rc.d HeaterControlCron defaults
+update-rc.d HeaterControlFront defaults
+update-rc.d HeaterControlBack defaults
