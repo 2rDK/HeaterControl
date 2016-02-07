7a4941e3db8663e4c565d3bdfb5a5d380317dbfe
2rDK <lemminger@gmail.com>
2016-02-07 11:54:20 +0100

    added more data for test

--- testPowerMeter.py
+++ testPowerMeter.py
@@ -3,8 +3,14 @@ instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
 instrument.serial.baudrate = 9600
 
 Vs = instrument.read_register(0,1)
+A = instrument.read_register(1,1)
 Fs = instrument.read_register(2,1)
+P = instrument.read_register(6,1)
+E = instrument.read_register(7,1)
 
 print("Supply voltage: "+str(Vs)+" Volt")
 print("Supply frequency: "+str(Fs)+" Hz")
+print("Current: "+str(A)+" Amp")
+print("Power: "+str(P)+" kW")
+print("Energy: "+str(E)+" kWh")
 print("\o/ yay !!")
