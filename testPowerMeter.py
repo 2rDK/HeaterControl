8f4aab126fd07e3532be828ecaedf9a39d58a98f
2rDK <lemminger@gmail.com>
2016-02-08 18:02:00 +0100

    init rewritten as driver

--- testPowerMeter.py
+++ testPowerMeter.py
@@ -1,18 +1,14 @@
 import minimalmodbus
-instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
-instrument.serial.baudrate = 9600
 
-Vs = instrument.read_register(0,1)
-A = instrument.read_register(1,1)
-Fs = instrument.read_register(2,1)
-Pf = instrument.read_register(6,1)
-E = instrument.read_register(7,1)
-Ti = instrument.read_register(25,1)
-
-print("Supply voltage: "+str(Vs)+" Volt")
-print("Supply frequency: "+str(Fs)+" Hz")
-print("Current: "+str(A)+" Amp")
-print("Power factor: "+str(Pf))
-print("Energy: "+str(E)+" kWh")
-print("Temp: "+str(Ti)+" C")
-print("\o/ yay !!")
+class D113003PowerMeter(minimalmodbus.Instrument):
+    
+    def __init__(self, portname, slaveaddress):
+        minimalmodbus.Instrument.__init__(self, portname, slaveaddress)
+        self.serial.baudrate = 9600
+    def get_energy(self):
+        """Return the process value (PV) for loop1."""
+        return self.read_long(7)
+    
+    
+pm = D113003PowerMeter('/dev/ttyUSB0', 1)
+print(pm.get_energy())
